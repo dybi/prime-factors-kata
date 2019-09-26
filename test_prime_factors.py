132d6bbbc710806728fbab5f5cc64264a8eee772
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-26 22:15:10 +0200

    Add failing test: `test_two_should_have_two_on_prime_factors_list`

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -6,3 +6,6 @@ from prime_factors import generate_prime_factors
 class TestGeneratePrimeFactors(TestCase):
     def test_one_should_have_empty_prime_factors_list(self):
         self.assertEqual(generate_prime_factors(1), [])
+
+    def test_two_should_have_two_on_prime_factors_list(self):
+        self.assertEqual(generate_prime_factors(2), [2])
