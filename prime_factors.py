11dbeddf5d0aa4c6fd9119ab536b20fa97a88715
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-26 22:21:08 +0200

    Make `test_three_should_have_three_on_prime_factors_list` pass with naive solution

--- prime_factors.py
+++ prime_factors.py
@@ -4,5 +4,5 @@ from typing import List
 def generate_prime_factors(number: int) -> List[int]:
     primes = []
     if number > 1:
-        primes.append(2)
+        primes.append(number)
     return primes
