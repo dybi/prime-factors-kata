a5709fae5b17e8b8ca74e7286b59bca994716080
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-26 22:30:48 +0200

    Make `test_eight_should_have_two_and_two_and_two_on_prime_factors_list` pass

--- prime_factors.py
+++ prime_factors.py
@@ -4,7 +4,7 @@ from typing import List
 def generate_prime_factors(number: int) -> List[int]:
     primes = []
     if number > 1:
-        if number % 2 == 0:
+        while number % 2 == 0:
             primes.append(2)
             number /= 2
         if number > 1:
