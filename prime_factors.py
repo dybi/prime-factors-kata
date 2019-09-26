c8f5cbf146cb8f8bf67dffaa7ae790be00927771
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-26 22:26:08 +0200

    Make `test_four_should_have_two_and_two_on_prime_factors_list` pass with naive solution
    Use nested `if` statements

--- prime_factors.py
+++ prime_factors.py
@@ -4,5 +4,9 @@ from typing import List
 def generate_prime_factors(number: int) -> List[int]:
     primes = []
     if number > 1:
-        primes.append(number)
+        if number % 2 == 0:
+            primes.append(2)
+            number /= 2
+        if number > 1:
+            primes.append(number)
     return primes
