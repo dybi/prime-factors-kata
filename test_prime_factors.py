3f5a8f9fbecfc7a0788bdc686372a5ac404e0b55
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-26 22:11:54 +0200

    Add failing test: `test_one_should_have_empty_prime_factors_list`

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 
+from prime_factors import generate_prime_factors
+
 
 class TestGeneratePrimeFactors(TestCase):
-    pass
+    def test_one_should_have_empty_prime_factors_list(self):
+        self.assertEqual(generate_prime_factors(1), [])
